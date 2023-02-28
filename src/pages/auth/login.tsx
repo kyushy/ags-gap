1bd6fd5f3d0babc6bde3c5f796ae0d6811c2c022
Frederic Testaniere <frederic.testaniere@meritis.fr>
2023-02-28 19:21:05 +0100

    add session status

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,14 +1,16 @@
 import type { Provider } from "next-auth/providers"
-import { getProviders, signIn } from "next-auth/react"
+import { getProviders, signIn, useSession } from "next-auth/react"
 import type { GetServerSideProps } from "next/types"
 import { getServerAuthSession } from "../../server/auth"
 
 
 export default function SignIn({ providers } : { providers : Provider[] }) {
+  const session = useSession()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <div>{session.status}</div>
       {Object.values(providers).map((provider) => (
         <div key={provider.name}>
           <button
