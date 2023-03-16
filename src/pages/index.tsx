668a4bd22850e32988c42c7a04c101cf08f2d0e9
Frederic Testaniere <frederic.testaniere@meritis.fr>
2023-03-16 17:37:17 +0100

    add images to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import { type NextPage } from "next";
 import { type GetServerSideProps } from "next/types"
 import Link from "next/link";
+import  Image from "next/image"
 import { signOut } from "next-auth/react";
 import { getServerAuthSession } from "../server/auth";
+import  logo  from '../assets/logo.png'
+import stock from '../assets/stocker2.png'
+import customer from '../assets/client.png'
 
 
 const Home: NextPage = () => {
@@ -10,31 +14,26 @@ const Home: NextPage = () => {
   return (
     <>
       <main className="flex flex-col items-center justify-center">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        <div className="container flex flex-col items-center gap-12 px-4 py-16 min-h-screen">
+          <Image src={logo} alt="logo" width={200}/>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-16 my-16">
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              className="flex max-w-xs flex-col gap-4 rounded-[1rem] bg-[#656F92] p-4 text-white hover:bg-white/20 border-[#211D58] border-4"
               href="/customers"
             >
-              <h3 className="text-2xl font-bold">Clients →</h3>
-              <div className="text-lg">
-                Gérer vos clients
-              </div>
+              <Image src={customer} alt="to_customer" width={180} className="p-6" />
             </Link>
             <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+              className="flex max-w-xs flex-col gap-4 rounded-[1rem] bg-[#656F92] p-4 text-white hover:bg-white/20 border-[#211D58] border-4"
               href="/stock"
             >
-              <h3 className="text-2xl font-bold">Stock →</h3>
-              <div className="text-lg">
-                Gérer votre stock
-              </div>
+              <Image src={stock} alt="to_stock" width={180} className="p-6" />
             </Link>
           </div>
           <div className="flex flex-col items-center gap-2">
             <div className="flex flex-col items-center justify-center gap-4">
               <button
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+                className="rounded-full bg-white/10 px-10 py-3 font-semibold text-black no-underline transition hover:bg-white/20 border-[#211D58] border-2"
                 onClick={() => signOut()}
               >
                 Se deconnecter
