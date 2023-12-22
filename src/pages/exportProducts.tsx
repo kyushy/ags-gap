2d5d5109d244a8f2ddf753e681bc060c3a5a51fa
unknown <kyush.spr@gmail.com>
2023-12-22 13:33:19 +0100

    added button to download empty stock list

--- src/pages/exportProducts.tsx
+++ src/pages/exportProducts.tsx
@@ -1,19 +1,24 @@
 import type { NextPage } from "next";
 import { api } from "../utils/api";
-import { Header } from "../components/layouts/header";
+import { usePDF, Margin } from "react-to-pdf";
+
 
 
 const ExportProducts : NextPage = () => {
     const { data } = api.product.getZeroQuantity.useQuery();
+    const { toPDF, targetRef } = usePDF({ filename: 'restock.pdf', method: 'open', page: { margin: Margin.MEDIUM } })
 
     return(
         <>
             <main className="flex flex-col justify-center">
                 <h1 className=" text-2xl text-center">Produit absent du stock</h1>
                 <div>
+                    <button onClick={() => toPDF()} type="button" className="float-right rounded-lg bg-[#18275B] px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300">Telecharger</button>
+                </div>   
+                <div ref={targetRef}>
                     {data?.map((item, index) => {
                         return(
-                            <div key={index}>{item.reference}  -  {item.name}</div>
+                            <div key={index} className="text-lg">{item.reference}  -  {item.name}</div>
                         )
                     })}
                 </div>
